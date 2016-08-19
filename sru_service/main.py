7949d0d7c03170901de5b7c636a29657f818812f
teezdan <teezdan@gmail.com>
2016-08-19 09:23:03 +0100

    added args handler

--- sru_service/main.py
+++ sru_service/main.py
@@ -10,6 +10,7 @@ from .settings import ROOT
 xml_template = os.path.join(ROOT, "service", "template.xml")
 xml_file = os.path.join(ROOT, "service", "sru.xml")
 service_file = os.path.join(ROOT, "service", "sru.exe")
+args_file = os.path.join(ROOT, "service", "args.txt")
 
 def setup(id, name, desc="", exe="python", arg="", logmode=""):
     with open(xml_template, "r", encoding="utf-8") as f:
@@ -17,6 +18,16 @@ def setup(id, name, desc="", exe="python", arg="", logmode=""):
         new_template = template.format(id, name, desc, exe, arg, logmode)
         with open(xml_file, "w+") as fr:
             fr.write(new_template)
+
+def setArgs(host="localhost", port="30080", ssl_cert=None, ssl_key=None, modules=None):
+    with open(args_file, "w+", encoding="utf-8") as f:
+        args = "-m sru.cli --host {} --port {} --cert {} --key {} --modules {}".format(host, port, ssl_cert, ssl_key, modules)
+        f.write(args)
+
+def getArgs():
+    with open(args_file, "r", encoding="utf-8") as f:
+        args = f.read()
+        return args
 
 def install():
     cmd = "{} install".format(service_file)
